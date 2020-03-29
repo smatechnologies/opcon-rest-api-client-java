7c7319755cc20c00d9556abc7a4a051335a87d68
Bertie van Hinsbergen <bvanhinsbergen@smasolutionsIT.com>
2020-03-29 18:02:15 +0200

    corrected error in json gen for WindowsPrioritySerializer

--- src/main/java/com/smatechnologies/opcon/restapiclient/jackson/serializer/WindowsPrioritySerializer.java
+++ src/main/java/com/smatechnologies/opcon/restapiclient/jackson/serializer/WindowsPrioritySerializer.java
@@ -17,17 +17,18 @@ public class WindowsPrioritySerializer extends JsonSerializer<WindowsDetails.Pri
 	public void serialize(Priority value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException {
         String apiVersion = (String) serializers.getAttribute(DefaultClientBuilder.VERSION_ATTRIBUTE);
 		
+        System.out.println("apiVersion (" + apiVersion + ")");
         if (value == null) {
            return;
         }
-        gen.writeStartObject();
         if (new VersionComparator().compare(apiVersion, "19.1.0") >= 0) {
+            gen.writeStartObject();
             gen.writeNumberField("id", value.getId());
             gen.writeStringField("name", value.getName());
+            gen.writeEndObject();
         } else { //Before 19.1.0
-            gen.writeNumberField("jobPriority", value.getId());
+            gen.writeNumber(value.getId());
         }
-        gen.writeEndObject();
 	}
 
 }
